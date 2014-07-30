563d15cfd09897a80051f20f3ab9d96e8fc060a3
Lim, Teck Hooi <teck-hooi.lim@hp.com>
2014-07-30 19:33:26 +0800

    1. Added simple multiple drink orders test suite. 2. Added multi threaded database insertions test case.

--- build.sbt
+++ build.sbt
@@ -8,4 +8,11 @@ libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
 
 libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.1.2"
 
-libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.3"
+libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.4"
+
+libraryDependencies += "junit" % "junit" % "4.4"
+
+libraryDependencies += "c3p0" % "c3p0" % "0.9.1.2"
+
+libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.31"
+
---
+++ src/test/java/TakingCoffeOrdersSuite.java
@@ -0,0 +1,129 @@
+import akka.dispatch.ExecutionContexts;
+import akka.dispatch.Futures;
+import akka.dispatch.OnFailure;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import javafx.util.Pair;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.concurrent.Await;
+import scala.concurrent.ExecutionContext;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static akka.dispatch.Futures.future;
+
+/**
+ * @author Lim, Teck Hooi
+ */
+
+public class TakingCoffeOrdersSuite {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TakingCoffeOrdersSuite.class);
+
+    static private DataSource dataSource;
+
+    private String[] customerNames = {
+        "Andrew", "James", "Adam",
+        "Kitty", "Alice", "Bob",
+        "Kathy"
+    };
+
+    @SuppressWarnings({"unchecked"})
+    private Pair<String, Double>[] drinks = new Pair[] {
+        new Pair("Latte", 10.0), new Pair("Cuppocino", 7.0), new Pair("Mocha", 11.0),
+        new Pair("Cola", 3.0), new Pair("Milk Shake", 6.0), new Pair("Lemon Juice", 5.0),
+        new Pair("Apple Juice", 5.0), new Pair("Water", 1.0),
+    };
+
+    private Random randomizer = new Random();
+
+    public String randomName() {
+        if (randomizer == null) {
+            randomizer = new Random();
+        }
+        return customerNames[randomizer.nextInt(customerNames.length - 1)];
+    }
+
+    public Pair<String, Double> randomDrink() {
+        if (randomizer == null) {
+            randomizer = new Random();
+        }
+        return drinks[randomizer.nextInt(drinks.length - 1)];
+    }
+
+    @BeforeClass
+    static public void commonSetup() throws Exception {
+        dataSource = new ComboPooledDataSource();
+    }
+
+    @Test
+    public void testInsertCoffeeOrders() throws Exception {
+
+        Connection conn = dataSource.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("insert into drink_order values(null, ?, ?, ?)");
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 1000; i++) {
+            stmt.setString(1, randomName());
+            Pair<String, Double> drink = randomDrink();
+            stmt.setString(2, drink.getKey());
+            stmt.setDouble(3, drink.getValue());
+            stmt.execute();
+        }
+        stmt.close();
+        LOGGER.debug("Insertion took " + (System.currentTimeMillis() - startTime) + "ms");
+
+        conn.close();
+    }
+
+    @Test
+    public void testFastInsertCoffeeOrders() throws Exception {
+        ExecutorService pool = Executors.newFixedThreadPool(18);
+        ExecutionContext ec = ExecutionContexts.fromExecutorService(pool);
+        List<Future<Boolean>> results = new ArrayList<>();
+
+        final Connection conn = dataSource.getConnection();
+        long startTime = System.currentTimeMillis();
+        final PreparedStatement stmt = conn.prepareStatement("insert into fast_drink_order values(null, ?, ?, ?)");
+
+        for (int i = 0; i < 1000; i++) {
+            Future<Boolean> result = future(new Callable<Boolean>() {
+                @Override
+                public Boolean call() throws Exception {
+                    stmt.setString(1, randomName());
+                    Pair<String, Double> drink = randomDrink();
+                    stmt.setString(2, drink.getKey());
+                    stmt.setDouble(3, drink.getValue());
+                    boolean result = stmt.execute();
+                    return result;
+                }
+            }, ec);
+            result.onFailure(new OnFailure() {
+                @Override
+                public void onFailure(Throwable failure) throws Throwable {
+                    LOGGER.error("Insertions failed.", failure);
+                }
+            }, ec);
+            results.add(result);
+        }
+
+        Await.ready(Futures.sequence(results, ec), Duration.create(5, TimeUnit.MINUTES));
+        LOGGER.debug("Insertion took " + (System.currentTimeMillis() - startTime) + "ms");
+
+        pool.shutdown();
+        stmt.close();
+        conn.close();
+    }
+}
